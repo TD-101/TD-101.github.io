8d4e84a43d9b27c99a6c663941136ae2902dcf6f
Thomas Dawson <tomdawson91@gmail.com>
2021-09-21 16:50:55 +1000

    initial commit of potabout

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    // navigate bar toggle
+    $('#navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(300);
+    });
+
+    function vh(v) {
+        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        return (v * h) / 100;
+      }
+    
+      // navbar bg change on scroll
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos > vh(50)){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
+});
