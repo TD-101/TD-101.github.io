4d5f49bcd15255edb3ae93b739124946a797effd
Thomas Dawson <tomdawson91@gmail.com>
2021-10-05 11:26:02 +1100

    navbar-collapse issues

--- js/script.js
+++ js/script.js
@@ -5,19 +5,25 @@ $(document).ready(function(){
         $('.navbar-collapse').slideToggle(300);
     });
 
+
+    $('.navbar-collapse>ul>li>a').click(function(){
+        $('.navbar-collapse').collapse('hide');
+
+    });
+
     function vh(v) {
         var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         return (v * h) / 100;
       }
     
       // navbar bg change on scroll
-    $(window).scroll(function(){
-        let pos = $(window).scrollTop();
-        if(pos > vh(50)){
-            $('.navbar').addClass('cng-navbar');
-        } else {
-            $('.navbar').removeClass('cng-navbar');
-        }
-    });
+    // $(window).scroll(function(){
+    //     let pos = $(window).scrollTop();
+    //     if(pos > vh(50)){
+    //         $('.navbar').addClass('cng-navbar');
+    //     } else {
+    //         $('.navbar').removeClass('cng-navbar');
+    //     }
+    // });
 
 });
