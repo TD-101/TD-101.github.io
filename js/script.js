dbe01010eaa498898753d731ff091ba5895dc114
Thomas Dawson <tomdawson91@gmail.com>
2021-10-12 15:25:32 +1100

    navbar broken again

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
+
 $(document).ready(function(){
 
     // navigate bar toggle
     $('#navbar-toggler').click(function(){
+        // document.getElementsByClassName('navbar-collapse').style.height = '100% !important';
+        // document.getElementsByClassName('navbar-nav').style.height = '100% !important';
         $('.navbar-collapse').slideToggle(300);
+        
     });
 
 
     $('.navbar-collapse>ul>li>a').click(function(){
         $('.navbar-collapse').slideToggle(300);
+         
     });
 
     function vh(v) {
         var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         return (v * h) / 100;
-      }
+    }
     
       // navbar bg change on scroll
     // $(window).scroll(function(){
@@ -24,5 +29,24 @@ $(document).ready(function(){
     //         $('.navbar').removeClass('cng-navbar');
     //     }
     // });
+
+    // var faq = document.getElementsByClassName("faq-page");
+    // var i;
+    
+    // for (i = 0; i < faq.length; i++) {
+    //     faq[i].addEventListener("click", function () {
+    //         /* Toggle between adding and removing the "active" class,
+    //         to highlight the button that controls the panel */
+    //         this.classList.toggle("active");
+    
+    //         /* Toggle between hiding and showing the active panel */
+    //         var body = this.nextElementSibling;
+    //         if (body.style.display === "block") {
+    //             body.style.display = "none";
+    //         } else {
+    //             body.style.display = "block";
+    //         }
+    //     });
+    // }
 
 });
